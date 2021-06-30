8ff7ca590c33945a0d07e7de6cd5a1ca58c90a5d
Andreas Beyer <abeyer@posteo.de>
2021-06-30 21:40:33 +0200

    add dev config

--- consts.js
+++ consts.js
@@ -1,4 +1,8 @@
+
+
+import { useProductionAPI } from './dev.config'
+
 export const SPOTIFY_CLIENT_ID = "5a8deb6692a34a738cf9386d357a7208";
-export const SPOTIBET_API_ENDPOINT = __DEV__
+export const SPOTIBET_API_ENDPOINT = __DEV__ && !useProductionAPI
   ? "http://localhost:4000"
   : "https://spotibet-api.herokuapp.com";
