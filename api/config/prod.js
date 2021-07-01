b92ca95cc2dffc67f44a1086f95703be03c86b07
Andreas Beyer <abeyer@posteo.de>
2021-07-01 22:07:35 +0200

    [api] debug prod.js

--- api/config/prod.js
+++ api/config/prod.js
@@ -5,5 +5,5 @@ module.exports = {
   apiJwtSecret: process.env.API_JWT_SECRET, // the secret which is used to create the api JWT e.g. secret
   statServerSecret: process.env.STAT_SERVER_SECRET, // the scret to communicate with the statserver e.g. secret // TODO: can be removed
   statServerURI: process.env.STAT_SERVER_URI, // http://localhost:5000 // TODO: can be removed
-  cronJobSecret: process.envt.CRON_JOB_SECRET // the secret to allow the cronJob server to access the api, unnecessary to setup in development
+  cronJobSecret: process.env.CRON_JOB_SECRET // the secret to allow the cronJob server to access the api, unnecessary to setup in development
 };
