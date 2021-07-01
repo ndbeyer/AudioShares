e0565db383503d4e3a974684df09bd8cc3603cfc
Andreas Beyer <abeyer@posteo.de>
2021-07-01 19:14:11 +0200

    change api name to audioshares

--- app/consts.js
+++ app/consts.js
@@ -5,4 +5,4 @@ import { useProductionAPI } from './dev.config'
 export const SPOTIFY_CLIENT_ID = "5a8deb6692a34a738cf9386d357a7208";
 export const SPOTIBET_API_ENDPOINT = __DEV__ && !useProductionAPI
   ? "http://localhost:4000"
-  : "https://spotibet-api.herokuapp.com";
+  : "https://audioshares-api.herokuapp.com";
