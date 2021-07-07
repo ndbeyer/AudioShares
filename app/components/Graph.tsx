3779e85c0d37ecf3d76a966e17d83cffe0e7c336
Andreas Beyer <abeyer@posteo.de>
2021-07-07 20:42:32 +0200

    Graph

---
+++ app/components/Graph.tsx
@@ -0,0 +1,111 @@
+//@format
+//@flow
+
+import React from 'react';
+import { useWindowDimensions } from 'react-native';
+import styled, { useTheme } from 'styled-components';
+import { differenceInCalendarWeeks } from 'date-fns';
+import { LineChart } from 'react-native-chart-kit';
+
+import { Paragraph } from './Text';
+import { getSuffix, correctNumberForSuffix } from '../util/suffix';
+import interpolate from '../util/interpolate';
+
+const Wrapper = styled.View`
+	margin: ${(p) => p.theme.rem2px(p.mar)};
+	border: 1px solid ${(p) => p.theme.colors.neutral4};
+	border-radius: ${(p) => 0.25 * p.theme.rem}px;
+	height: ${(p) => p.theme.rem2px('8rem')};
+	max-width: 100%;
+	align-self: stretch;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Graph = ({
+	data = [],
+	margin = '2rem 2rem',
+	pxHeight = 220,
+}: {
+	data: { id: string; dateTime: string; monthlyListeners: number }[];
+	margin: string;
+	pxHeight: number;
+}): React.Element => {
+	const theme = useTheme();
+	const { width: pxWidth } = useWindowDimensions();
+	const [pxMarginTop, pxMarginRight, pxMarginBottom, pxMarginLeft] = theme.rem24sidesPx(
+		margin,
+		'pxArray'
+	);
+	const pxChartWidth = React.useMemo(() => pxWidth - pxMarginRight - pxMarginLeft, [
+		pxMarginLeft,
+		pxMarginRight,
+		pxWidth,
+	]);
+
+	const suffix = React.useMemo(() => getSuffix(data?.[data.length - 1]?.monthlyListeners), [data]);
+	const today = React.useMemo(() => new Date(), []);
+	const interpolatedData = React.useMemo(() => interpolate(data), [data]);
+
+	const decimalPlaces = React.useMemo(() => {
+		if (interpolatedData?.[0]?.monthlyListeners) {
+			return (
+				3 -
+				Number(correctNumberForSuffix(interpolatedData?.[0]?.monthlyListeners, suffix)).toFixed()
+					.length
+			);
+		}
+		return 0;
+	}, [interpolatedData, suffix]);
+
+	return !interpolatedData ? (
+		<Wrapper mar={margin}>
+			<Paragraph color="$neutral3">No history data available</Paragraph>
+		</Wrapper>
+	) : (
+		<>
+			<LineChart
+				// eslint-disable-next-line react-perf/jsx-no-new-object-as-prop
+				data={{
+					labels: interpolatedData.map(({ dateTime }) =>
+						differenceInCalendarWeeks(new Date(dateTime), today)
+					),
+					datasets: [
+						{
+							data: interpolatedData.map(({ monthlyListeners }) =>
+								correctNumberForSuffix(monthlyListeners, suffix)
+							),
+						},
+					],
+				}}
+				width={pxChartWidth}
+				height={pxHeight}
+				withDots={false}
+				yAxisSuffix={suffix}
+				xAxisInteval={0}
+				// eslint-disable-next-line react-perf/jsx-no-new-object-as-prop
+				chartConfig={{
+					backgroundGradientFrom: theme.colors.background0,
+					backgroundGradientTo: theme.colors.background0,
+					decimalPlaces,
+					color: (opacity = 1) => theme.colors.neutral1,
+					labelColor: (opacity = 1) => theme.colors.neutral1,
+					fillShadowGradient: theme.colors.accent0,
+					strokeWidth: 0.5,
+					propsForBackgroundLines: {
+						strokeWidth: '0',
+					},
+					propsForLabels: {},
+				}}
+				// eslint-disable-next-line react-perf/jsx-no-new-object-as-prop
+				style={{
+					marginTop: pxMarginTop,
+					marginBottom: pxMarginBottom,
+				}}
+			/>
+			<Paragraph margin="-2rem 0 0 0">Weeks</Paragraph>
+		</>
+	);
+};
+
+export default Graph;
---
+++ app/components/ThemeProvider2.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { ThemeProvider as SNThemeProvider } from 'styled-components';
+import { times } from 'lodash';
+import { UNIT, colorDefs, typoDefs, pathDefs } from '../util/theme';
+
+export const createVariables = ({ baseBorderRadius = 0.25 } = {}) => {
+	return {
+		...Object.assign(
+			...times(5, (i) => ({
+				[`borderRadius${i}`]: baseBorderRadius * Math.pow(2, i) + 'rem',
+			}))
+		),
+		...typoDefs(),
+	};
+};
+
+// PortalProvider requires styled-native-components, so it is imported after calling setStaticVariables(..)
+import PortalProvider from './PortalProvider';
+
+const theme = {
+	rem: UNIT,
+	iconPaths: pathDefs(),
+	colors: colorDefs({ accentColor: '#34eb46', chromaticity: 50 }),
+	elevation: (value) => ({
+		shadowColor: 'black',
+		shadowOffset: { width: 0, height: value },
+		shadowRadius: value * 2.5,
+		shadowOpacity: 0.3,
+		elevation: value,
+		zIndex: value,
+	}),
+	rem2px: (string) =>
+		string
+			.split(' ')
+			.map((str) => Number(str.replace('rem', '')))
+			.reduce((a, b) => `${a} ${b * UNIT}px`, ''),
+	rem24sidesPx: (string: string, type: 'pxString' | 'pxArray' = 'pxString') => {
+		const array = [];
+		string.split(' ').forEach((str) => array.push(Number(str.replace('rem', ''))));
+		if (array.length === 1) {
+			times(3).forEach((_) => array.push(array[0]));
+		} else if (array.length === 2) {
+			array.push(array[0]);
+			array.push(array[1]);
+		} else if (array.length === 3) {
+			array.push(array[1]);
+		} else {
+			// do nothing
+		}
+		return type === 'pxString'
+			? array.reduce((a, b) => `${a} ${b * UNIT}px`, '')
+			: array.map((n) => n * UNIT);
+	},
+};
+
+const ThemeProvider = ({ children }) => {
+	return (
+		<SNThemeProvider theme={theme}>
+			<PortalProvider>{children}</PortalProvider>
+		</SNThemeProvider>
+	);
+};
+
+export default ThemeProvider;
