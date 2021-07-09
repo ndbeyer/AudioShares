bb0166c08c3ec55fc4696f3caec9b8c22ed41622
Andreas Beyer <abeyer@posteo.de>
2021-07-09 19:18:57 +0200

    next

---
+++ app/screens/LoginScreen.tsx
@@ -0,0 +1,93 @@
+//@format
+//@flow
+
+import React from 'react';
+import styled from 'styled-components';
+
+import Button from '../components/Button';
+import Screen from '../components/Screen';
+import Loading from '../components/Loading';
+import { Heading, Label, Paragraph } from '../components/Text';
+
+import { login } from '../state/auth';
+import { usePortal } from '../components/PortalProvider';
+
+const StyledScreen = styled(Screen)`
+	justify-content: center;
+`;
+
+const loginErrorDescriptions = {
+	GET_JWT_FOR_AUTH_CODE_ERROR: 'Unexpected Server error GET_JWT_FOR_AUTH_CODE_ERROR',
+	NO_JWT: 'Unexpected Server Error NO_JWT',
+	FETCH_USER_ERROR: 'Unexpected Server Error FETCH_USER_ERROR',
+	NETWORK_ERROR: 'Network error. You seem to be offline.',
+	ACCESS_DENIED: 'You canceled the spotify login process. Please try again.',
+	CANCELLED_APP_AUTH: 'You canceled the spotify login process. Please try again.',
+};
+
+const LoginScreen = (): React.Element => {
+	const [clicked, setClicked] = React.useState(false);
+	const [loading, setLoading] = React.useState(false);
+
+	const { renderPortal } = usePortal();
+
+	const handleError = React.useCallback(
+		(errorCode) => {
+			renderPortal({
+				title: 'Error',
+				description: loginErrorDescriptions[errorCode] || 'Unexpected Server Error',
+			});
+		},
+		[renderPortal]
+	);
+
+	const handleLogin = React.useCallback(async () => {
+		setLoading(true);
+		const { success, error } = await login();
+		if (!success) {
+			handleError(error);
+			setLoading(false);
+		}
+	}, [handleError]);
+
+	React.useEffect(() => setLoading(false), []);
+
+	const [handleClickTrue, handleClickFalse] = React.useMemo(
+		() => [() => setClicked(true), () => setClicked(false)],
+		[]
+	);
+
+	return (
+		<StyledScreen>
+			{!clicked ? (
+				<>
+					<Heading size="xl">SpotiBet</Heading>
+					<Label size="m" uppercase margin="0 0 4rem">
+						Good taste, good money
+					</Label>
+					<Button onPress={handleClickTrue} label="Start" />
+				</>
+			) : loading ? (
+				<Loading />
+			) : (
+				<>
+					<Heading size="xl">SpotiBet</Heading>
+					<Paragraph align="center" margin="1rem 4rem 1.5rem" size="m" uppercase>
+						To use this app, it is necessary that you login with your spotify account. You will be
+						asked to enter your spotify credentials in the next step.
+					</Paragraph>
+
+					<Button onPress={handleLogin} label="Ok" />
+					<Button
+						margin="0"
+						backgroundColor="$background0"
+						onPress={handleClickFalse}
+						label="Cancel"
+					/>
+				</>
+			)}
+		</StyledScreen>
+	);
+};
+
+export default LoginScreen;
---
+++ app/screens/Navigator.tsx
@@ -0,0 +1,162 @@
+//@format
+//@flow
+
+import React from 'react';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Icon from '../components/Icon';
+import Loading from '../components/Loading';
+import { Label } from '../components/Text';
+import InitializingScreen from './InitializingScreen';
+import LoginScreen from './LoginScreen';
+import DashboardScreen from './DashboardScreen';
+import PlaylistScreen from './PlaylistScreen';
+import ArtistsOfPlaylistScreen from './ArtistsOfPlaylistScreen';
+import ArtistScreen from './ArtistScreen';
+import TransactionsScreen from './TransactionsScreen';
+import SettingsScreen from './SettingsScreen';
+import ArtistBetsScreen from './ArtistBetsScreen';
+
+import { useUser, fetchUser } from '../state/user';
+import { refreshLogin } from '../state/auth';
+import { makeUserBetTransactions } from '../state/user';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const headerShownFalseOptions = { headerShown: false };
+
+const CreateStack = (): React.Element => {
+	return (
+		<Stack.Navigator
+			initialRouteName="Playlists"
+			// eslint-disable-next-line react-perf/jsx-no-new-object-as-prop
+			screenOptions={{
+				headerTitle: (options) => {
+					return (
+						<Label light size="xl">
+							{options.children}
+						</Label>
+					);
+				},
+			}}
+		>
+			<Stack.Screen name="Playlists" component={PlaylistScreen} />
+			<Stack.Screen name="Artists" component={ArtistsOfPlaylistScreen} />
+			<Stack.Screen name="Artist" component={ArtistScreen} options={headerShownFalseOptions} />
+			<Stack.Screen
+				name="ArtistBetsScreen"
+				component={ArtistBetsScreen}
+				options={headerShownFalseOptions}
+			/>
+		</Stack.Navigator>
+	);
+};
+
+const useAppState = () => {
+	const { currentUser, loading } = useUser();
+
+	const [triedRefresh, setTriedRefresh] = React.useState(false);
+
+	React.useEffect(() => {
+		(async () => {
+			const { success } = await refreshLogin();
+			if (success) {
+				await fetchUser();
+			}
+			setTriedRefresh(true);
+		})();
+	}, []);
+
+	if (loading || (!loading && !triedRefresh)) return 'LOADING';
+	if (currentUser) return 'LOGGED_IN';
+	else return 'LOGGED_OUT';
+};
+
+const routeIcons = {
+	Dashboard: 'profile',
+	Create: 'play',
+	Transactions: 'graph',
+	Settings: 'gear',
+};
+
+const LoggedInNavigator = () => {
+	const [loading, setLoading] = React.useState(true);
+
+	React.useEffect(() => {
+		(async () => {
+			const { success, error } = await makeUserBetTransactions();
+			console.log('makeUserBetTransactions', { success, error }); // TODO: remove
+			setLoading(false);
+		})();
+	}, []);
+
+	return loading ? (
+		<Loading />
+	) : (
+		<>
+			<Tab.Navigator
+				initialRouteName="Create"
+				// eslint-disable-next-line react-perf/jsx-no-new-object-as-prop
+				tabBarOptions={{
+					showLabel: false,
+				}}
+				// eslint-disable-next-line react-perf/jsx-no-new-function-as-prop
+				screenOptions={({ route }) => ({
+					tabBarIcon: ({ focused }) => (
+						<Icon
+							size="3rem"
+							name={routeIcons[route.name]}
+							color={focused ? 'neutral0' : 'neutral4'}
+						/>
+					),
+				})}
+			>
+				<Tab.Screen name="Dashboard" component={DashboardScreen} />
+				<Tab.Screen name="Create" component={CreateStack} />
+				<Tab.Screen name="Transactions" component={TransactionsScreen} />
+				<Tab.Screen name="Settings" component={SettingsScreen} />
+			</Tab.Navigator>
+		</>
+	);
+};
+
+const Navigator = (): React.Element => {
+	const appState = useAppState();
+	console.log('appState', appState);
+
+	return (
+		// Navigator does not re-render too often, only if appState or theme changes
+		// if Navigator will re-render, caching the options objects in this case, will make the Screens to not re-render IF the Screens return a memoized UI
+		<NavigationContainer>
+			{appState === 'LOGGED_OUT' ? (
+				<>
+					<Stack.Navigator>
+						<Stack.Screen
+							name="Login"
+							component={LoginScreen}
+							// eslint-disable-next-line react-perf/jsx-no-new-object-as-prop
+							options={{ headerShown: false }}
+						/>
+					</Stack.Navigator>
+				</>
+			) : appState === 'LOGGED_IN' ? (
+				<LoggedInNavigator />
+			) : (
+				<Stack.Navigator>
+					<Stack.Screen
+						name="Initializing"
+						component={InitializingScreen}
+						// eslint-disable-next-line react-perf/jsx-no-new-object-as-prop
+						options={{ headerShown: false }}
+					/>
+				</Stack.Navigator>
+			)}
+		</NavigationContainer>
+	);
+};
+
+export default Navigator;
